fcc3dd7c5998d7189a0e9ebd25a33bd668cd9b9d
Kevin Bluett <kevin.bluett@gmail.com>
2012-07-03 12:52:47 +0100

    Improved tests for the changes to the server. Tests only work when Cherrypy or Twisted is enabled on the server

--- remotetest.py
+++ remotetest.py
@@ -3,8 +3,10 @@ import argparse
 import sys
 from colorama import init
 from termcolor import colored
+from StringIO import StringIO
 import re as regex
 import json
+from json import load
 
 """
 remotetest
@@ -68,10 +70,12 @@ def main ():
 		(resp_headers, content) = h.request(args.server + args.testpath, "GET")	
 		if content.rstrip()  in args.test_json.rstrip():
 			print "TEST " + colored(testcount, "blue") + ": "  + colored("Successful", "green")
-			successful = successful + 1
+			successful += 1
 		else:
 			print"TEST " + colored(testcount, "blue") + ": " + colored("Failed", "red")
 			print "\tREASON: " + colored("Returned JSON does not equal sample JSON", "red")
+			print "\tReturned JSON: " + colored(content, "yellow")
+
 			failed = failed + 1
 	except:
 		print"TEST " + colored(testcount, "blue") + ": " + colored("Failed", "red")
@@ -81,13 +85,28 @@ def main ():
 	testcount = testcount +1
 	print "\nTEST " + colored(testcount, "blue") + ": " + "Attempting to get API version from server..."
 	try:
-		(resp_headers, content) = h.request(args.server + "api/version", "GET", '{ "os":"android", "version":"2.3.5","appversion":"0.1"}')
-		if resp_headers. status != 400:
+		(resp_headers, content) = h.request(args.server + "version" + "/", "GET", '{ "os":"android", "version":"2.3.5","appversion":"0.1"}')
+		
+		if resp_headers.status == 200:
 			# TO DO: How does server return API versions?
-			print "TODO"
-		else:
+
+			mapped = load(StringIO(content))
+
+			print "TEST " + colored(testcount, "blue") + ": "  + colored("Successful", "green")
+
+			successful += 1
+
+			if 'version' in mapped:
+				print "\tServer Version: " + colored(mapped['version'], "yellow")
+
+		elif resp_headers.status >= 400 and resp_headers.status < 500:
 			print"TEST " + colored(testcount, "blue") + ": " + colored("Failed", "red")
 			print "\tREASON: " + colored("Server did not understand API version request", "red")
+			failed = failed + 1
+
+		elif resp_headers.status >= 500:
+			print"TEST " + colored(testcount, "blue") + ": " + colored("Failed", "red")
+			print "\tREASON: " + colored("Server had encountered internal server", "red")
 			failed = failed + 1
 	except:
 		print"TEST " + colored(testcount, "blue") + ": " + colored("Failed", "red")
