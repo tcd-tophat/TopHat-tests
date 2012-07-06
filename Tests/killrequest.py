1fab17d089acaace5d8e3956bc9f357a36573bd2
Kevin Bluett <kevin.bluett@gmail.com>
2012-07-06 14:21:13 +0100

    New tests for new requests

---
+++ Tests/killrequest.py
@@ -0,0 +1,26 @@
+from test import Test
+
+class Killrequest(Test):
+
+	short_title = "Server Kill Information Request"
+	title = "Attempts to get information on a kill from the server."
+
+	KILL_ID = 1
+
+	def _test(self):
+		(self.headers, self.content) = self.h.request(self.args.server + "kills" + "/" + str(self.KILL_ID), "GET", '')
+
+	def _request_ok(self, content):
+		if self._status == 200 and "killer" in content:
+			if content['id'] == self.KILL_ID:
+				self.inform = "Kill Info Received - id: "+str(content['id'])+" - victim:  "+str(content['victim']['name'])
+				return True
+			else:
+				self._reason = "The server did not return the same kill as request "+str(self.KILL_ID)+" != "+str(content['id'])
+				return False
+		elif self._status == 404:
+			self._reason = "The kill at the ID of "+str(self.KILL_ID)+" does not exist"
+			return False
+		else:
+			self._reason = "Missing JSON or Invalid Status Code."
+			return False
---
+++ Tests/killrequestlist.py
@@ -0,0 +1,17 @@
+from test import Test
+
+class Killrequestlist(Test):
+
+	short_title = "Server Kill Listings Information"
+	title = "Attempts to get a list of kills from the server."
+
+	def _test(self):
+		(self.headers, self.content) = self.h.request(self.args.server + "kills" + "/", "GET", '')
+
+	def _request_ok(self, content):
+		if self._status == 200 and "kills" in content:
+			self.inform = "Kill Listings Received - Amount of Available Games: "+str(len(content["kills"]))
+			return True
+		else:
+			self._reason = "Missing JSON or Invalid Status Code."
+			return False
