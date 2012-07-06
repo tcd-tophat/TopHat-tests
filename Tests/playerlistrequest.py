469c887f80ffbe97bc335d3b52524f49b0121bdb
Kevin Bluett <kevin.bluett@gmail.com>
2012-07-06 14:54:24 +0100

    Created new tests for the player requests

---
+++ Tests/playerlistrequest.py
@@ -0,0 +1,17 @@
+from test import Test
+
+class Playerlistrequest(Test):
+
+	short_title = "Server Player Listings Information"
+	title = "Attempts to get a list of games from the server."
+
+	def _test(self):
+		(self.headers, self.content) = self.h.request(self.args.server + "players" + "/", "GET", '')
+
+	def _request_ok(self, content):
+		if self._status == 200 and "players" in content:
+			self.inform = "PLayers Listings Received - Amount of Available Games: "+str(len(content["players"]))
+			return True
+		else:
+			self._reason = "Missing JSON or Invalid Status Code."
+			return False
---
+++ Tests/playerrequest.py
@@ -0,0 +1,26 @@
+from test import Test
+
+class Playerrequest(Test):
+
+	short_title = "Server Player Information Request"
+	title = "Attempts to get information on a game from the server."
+
+	PLAYER_ID = 1
+
+	def _test(self):
+		(self.headers, self.content) = self.h.request(self.args.server + "players" + "/" + str(self.PLAYER_ID), "GET", '')
+
+	def _request_ok(self, content):
+		if self._status == 200 and "name" in content and "id" in content:
+			if content['id'] == self.PLAYER_ID:
+				self.inform = "Player Info Received - id: "+str(content['id'])+" - name:  "+content['name']
+				return True
+			else:
+				self._reason = "The server did not return the same game as request "+str(self.PLAYER_ID)+" != "+str(content['id'])
+				return False
+		elif self._status == 404:
+			self._reason = "The player at the ID of "+str(self.PLAYER_ID)+" does not exist"
+			return False
+		else:
+			self._reason = "Missing JSON or Invalid Status Code."
+			return False
