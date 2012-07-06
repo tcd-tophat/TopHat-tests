7b762482d3071a4000a861837252f57f191c40d9
Kevin Bluett <kevin.bluett@gmail.com>
2012-07-06 13:06:13 +0100

    Game listing test

---
+++ Tests/gamelistrequest.py
@@ -0,0 +1,17 @@
+from test import Test
+
+class Gamelistrequest(Test):
+
+	short_title = "Server Game Listings Information"
+	title = "Attempts to get a list of games from the server."
+
+	def _test(self):
+		(self.headers, self.content) = self.h.request(self.args.server + "games" + "/", "GET", '')
+
+	def _request_ok(self, content):
+		if self._status == 200 and "games" in content:
+			self.inform = "Game Listings Received - Amount of Available Games: "+str(len(content["games"]))
+			return True
+		else:
+			self._reason = "Missing JSON or Invalid Status Code."
+			return False
