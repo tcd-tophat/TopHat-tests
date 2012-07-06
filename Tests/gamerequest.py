25b136afea443db131533f6ea685295f9df3649e
Kevin Bluett <kevin.bluett@gmail.com>
2012-07-06 12:36:55 +0100

    improved the game request a little more for the static variable

--- Tests/gamerequest.py
+++ Tests/gamerequest.py
@@ -5,21 +5,21 @@ class Gamerequest(Test):
 	short_title = "Server Game Information Request"
 	title = "Attempts to get information on a game from the server."
 
-	game_id = 1
+	GAME_ID = 1
 
 	def _test(self):
-		(self.headers, self.content) = self.h.request(self.args.server + "games" + "/" + self.game_id, "GET", '')
+		(self.headers, self.content) = self.h.request(self.args.server + "games" + "/" + str(self.GAME_ID), "GET", '')
 
 	def _request_ok(self, content):
 		if self._status == 200 and "name" in content and "id" in content:
-			if content['id'] == self.game_id:
+			if content['id'] == self.GAME_ID:
 				self.inform = "Game Info Received - id: "+str(content['id'])+" - name:  "+content['name']
 				return True
 			else:
-				self._reason = "The server did not return the same game as request "+str(self.game_id)+" != "+str(content['id'])
+				self._reason = "The server did not return the same game as request "+str(self.GAME_ID)+" != "+str(content['id'])
 				return False
 		elif self._status == 404:
-			self._reason = "The game at the ID of "+str(self.game_id)+" does not exist"
+			self._reason = "The game at the ID of "+str(self.GAME_ID)+" does not exist"
 			return False
 		else:
 			self._reason = "Missing JSON or Invalid Status Code."
