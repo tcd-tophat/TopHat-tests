c50118d2722cdccb08100eb44fe6e87404dd73f4
specialkbyte <eire32kevin@gmail.com>
2012-09-09 22:52:57 +0100

    Added kill create and player create (POST) requests in the test suites

---
+++ Tests/killcreate.py
@@ -0,0 +1,24 @@
+from test import Test
+from urllib import urlencode
+
+class Killcreate(Test):
+
+	short_title = "Create Kill Request"
+	title = "Attempts to create a new kill on the server"
+
+	def _test(self):
+		url = self.args.server + "kills/?apitoken=" + self._getApiToken()
+		dict_ = {}
+		dict_["data"] = '{"killer": {"id": 1}, "victim_qrcode": "RCcEKo"}'
+		data = urlencode(dict_)
+		(headers, content) = self.h.request(url, method="POST", body=data, headers={'Content-type': 'application/x-www-form-urlencoded'})
+		self.headers = headers
+		self.content = content	
+
+	def _request_ok(self, content):
+		if self._status == 201 and "name" in content and "id" in content:
+			self.inform = "The game was created successfully"
+			return True
+		else:
+			self._reason = "Missing JSON or Invalid Status Code."
+			return False
